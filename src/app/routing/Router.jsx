b24e5ab83a68ff3cfc2829352ff4c14638533e6f
Mary Rastorgueva <mari.rast4722@gmail.com>
2026-07-15 14:00:45 +0300

    fix: update import path

--- src/app/routing/Router.jsx
+++ src/app/routing/Router.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { BASE_URL } from "../../shared/constants"
+import { BASE_URL } from "@/shared/constants"
 
 const getCurrentPath = () => {
     const pathName = window.location.pathname
